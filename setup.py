e13024d29789ab81135408e562eeca8f93e0937e
YanMo <xiajiaruimail@qq.com>
2024-06-08 20:57:00 +0800

    feat(setup.py): setup initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="locyan_launcher",
+    version="1.0.0",
+    author="YanMo",
+    author_email="xiajiaruimail@qq.com",
+    description="Python 库 - 乐青 Frp 启动器",
+    url="https://github.com/ltzXiaoYanMo/LoCyanFrpLauncher",
+    requires=["requests"],
+    packages=find_packages(),
+    license="MIT License",
+    long_description=open("README.md", "r", encoding="UTF-8").read(),
+    python_requires=">=3.6"
+)
